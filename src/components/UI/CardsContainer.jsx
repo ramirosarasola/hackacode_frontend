6f01267474c267d57e79768c4dc7678ecdefecc7
mateoghidini1998 <mateghidini123@gmail.com>
2023-06-12 16:01:15 -0300

    rendered dynamically the cards at home page

--- src/components/UI/CardsContainer.jsx
+++ src/components/UI/CardsContainer.jsx
@@ -2,21 +2,36 @@ import React from 'react'
 import ModuleCard from './ModuleCard'
 import '../../styles/components/UI/CardsContainer.css'
 
-function CardsContainer() {
+
+const modules = [
+  {
+  title: 'Users',
+  subheading: 'See all Users'
+  },
+  {
+    title: 'Employees',
+    subheading: 'See all Employees'
+  },
+  {
+    title: 'Customers',
+    subheading: 'See all Customers'
+  },
+  {
+    title: 'Games',
+    subheading: 'See all Games'
+  },
+  {
+    title: 'Sales',
+    subheading: 'See all Sales'
+  },
+]
+
+function CardsContainer({props}) {
   return (
     <div className='cards-container'>
-        <div className="row">
-            <ModuleCard/>
-            <ModuleCard/>
-        </div>
-        <div className="row">
-            <ModuleCard/>
-            <ModuleCard/>
-        </div>  
-        <div className="row">
-            <ModuleCard/>
-            <ModuleCard/>
-        </div>      
+        {modules.map(module => (
+          <ModuleCard title={module.title} subheading={module.subheading} props={props} key={module.title}/>
+        ))}
     </div>
   )
 }
--- src/components/UI/ModuleCard.jsx
+++ src/components/UI/ModuleCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import '../../styles/components/UI/ModuleCard.css'
 import { Engineering as EngineeringIcon } from  "@mui/icons-material";
 
-function ModuleCard() {
+function ModuleCard({ title, subheading }) {
   return (
     <div className='card-container'>
         <div className="card-heading">
-            <h2>EMPLOYEES</h2>
+            <h2>{title}</h2>
             <EngineeringIcon className='icon'/>
         </div>
-        <p className='subheading'>See all employees</p>
+        <p className='subheading'>{subheading}</p>
     </div>
   )
 }
